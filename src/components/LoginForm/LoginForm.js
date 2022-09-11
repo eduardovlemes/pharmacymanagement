702d8fc786ef7f775f81e557e11e824b0fe50f07
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-11 18:56:01 -0300

    feat: adiciona função HandleSubmit no LoginForm
    Adiciona a função ao LoginForm para fazer as validações adicionais aos inputs.

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -4,10 +4,36 @@ export default function LoginForm() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
+  function HandleSubmit(event) {
+    event.preventDefault();
+    if (!email) {
+      alert("Insira um email.");
+      return;
+    } else if (
+      !email.match(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      )
+    ) {
+      alert("Digite um e-mail válido.");
+      return;
+    }
+
+    if (!password) {
+      alert("Insira a senha.");
+      return;
+    } else if (password.length < 8) {
+      alert("A senha deve conter no mínimo 8 caracteres.");
+      return;
+    } else if (!password.match(/^[a-zA-Z0-9-]/)) {
+      alert("A senha deve conter apenas letras e/ou números.");
+      return;
+    }
+  }
+
   return (
     <>
       <h2>Login</h2>
-      <form>
+      <form onSubmit={HandleSubmit}>
         <label>
           E-mail
           <input
