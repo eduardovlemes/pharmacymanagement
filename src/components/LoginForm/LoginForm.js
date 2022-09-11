dead733ba0238e91906d9916efcad22d043fa81b
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-11 18:50:20 -0300

    feat: adiciona useState no LoginForm
    Adiciona o useState no email e no password do LoginForm.

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,16 +1,32 @@
+import { useState } from "react";
+
 export default function LoginForm() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <>
       <h2>Login</h2>
       <form>
         <label>
           E-mail
-          <input required type="email" placeholder="exemplo@mail.com" />
+          <input
+            required
+            type="email"
+            placeholder="exemplo@mail.com"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </label>
 
         <label>
           Senha
-          <input required type="password" />
+          <input
+            required
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </label>
         <button type="submit">Entrar</button>
         <button>Cadastrar usuário</button>
