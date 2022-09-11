0900601b6fd025f0a92ade8ebff855ad274195fe
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-11 16:45:44 -0300

    feat: cria o script do componente DrugForm
    Cria os inputs e botões do componente DrugForm

--- src/components/DrugForm/DrugForm.js
+++ src/components/DrugForm/DrugForm.js
@@ -1,3 +1,34 @@
 export default function DrugForm() {
-  return;
+  return (
+    <>
+      <form>
+        <label>
+          Medicamento <input required />
+        </label>
+
+        <label>
+          Laboratório <input required />
+        </label>
+
+        <label>
+          Dosagem <input required />
+        </label>
+
+        <label>
+          Tipo <input required />
+        </label>
+
+        <label>
+          Preço Unitário <input required />
+        </label>
+
+        <label>
+          Descrição <input required />
+        </label>
+
+        <button>Limpar</button>
+        <button>Salvar</button>
+      </form>
+    </>
+  );
 }
