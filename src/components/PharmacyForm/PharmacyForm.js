501c6ed65e4d3dc395768993be9b1eb038fc6325
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-11 16:40:06 -0300

    feat: Cria o script do componente PharmacyForm
    Cria os inputs e botões do componente PharmacyForm.

--- src/components/PharmacyForm/PharmacyForm.js
+++ src/components/PharmacyForm/PharmacyForm.js
@@ -1,3 +1,71 @@
 export default function PharmacyForm() {
-  return;
+  return(
+    <>
+    <form>
+      <label>
+        Razão Social <input required />
+      </label>
+
+      <label>
+        CNPJ <input required />
+      </label>
+
+      <label>
+        Nome Fantasia <input required />
+      </label>
+
+      <label>
+        E-mail <input required />
+      </label>
+
+      <label>
+        Telefone <input required />
+      </label>
+
+      <label>
+        Celular <input required />
+      </label>
+
+      <label>Endereço</label>
+      <label>
+        CEP <input required />
+      </label>
+
+      <label>
+        Logradouro <input required />
+      </label>
+
+      <label>
+        Número <input required />
+      </label>
+
+      <label>
+        Bairro <input required />
+      </label>
+
+      <label>
+        Cidade <input required />
+      </label>
+
+      <label>
+        Estado <input required />
+      </label>
+
+      <label>
+        Complemento <input />
+      </label>
+
+      <label>
+        Latitude <input required />
+      </label>
+
+      <label>
+        Longitude <input required />
+      </label>
+
+      <button>Limpar</button>
+      <button>Salvar</button>
+    </form>
+    </>
+  );
 }
