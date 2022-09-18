5662d0da954a1238700eb67e5992d10eb6f4ee49
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-18 16:01:27 -0300

    feat: adiciona useState e useEffect
    Adiciona useState e useEffect no componente DrugItem e apresenta na tela os itens cadastrados

--- src/components/DrugItem/DrugItem.js
+++ src/components/DrugItem/DrugItem.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+
 export default function DrugItem() {
+  const [drugs, setDrugs] = useState([]);
+
+  useEffect(() => {
+    async function getDrugs() {
+      await fetch("http://localhost:3001/drugs")
+        .then((response) => response.json())
+        .then((dataFromDrugsServer) => {
+          setDrugs(dataFromDrugsServer);
+        });
+    }
+    getDrugs();
+  }, []);
+
   return (
     <>
-      <img alt="i"></img>
-      <span>Medicamento</span>
-      <span>Dosagem</span>
-      <p>Laboratório</p>
+      <h2>Medicamentos cadastrados</h2>
+      <ul>
+        {drugs.map((item) => (
+          <li>
+            <img alt="drug" src={item.imgDrug} />
+            <div>
+              <span>{item.drugName}</span>
+              <span>{item.dosage}</span>
+              <p>R$ {item.price}</p>
+              <p>{item.lab}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
