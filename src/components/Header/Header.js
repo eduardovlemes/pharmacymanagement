edfa6f09f231c960351a2caf5824a5589a406ffe
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-19 15:37:40 -0300

    feat : cria estrutura do componente
    cria a estrutura do componente que será usado nas demais páginas do projeto

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,28 @@
+import { useNavigate } from "react-router";
+import logo from "../../assets/logo.png";
+
 export default function Header() {
-  return;
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    if (window.confirm("Deseja sair?")) {
+      navigate("/");
+    }
+  }
+
+  return (
+    <header>
+      <nav>
+        <img
+          alt="logo"
+          src={logo}
+          width="200"
+          onClick={() => navigate("/mapa")}
+        ></img>
+        <div>
+          <button onClick={handleLogout}>Sair</button>
+        </div>
+      </nav>
+    </header>
+  );
 }
