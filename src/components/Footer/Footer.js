cbcada396397bf24f54b6b1d27d6a2dc023e6ca0
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-19 16:57:11 -0300

    feat: cria estrutura do Footer
    Cria a estrutura do footer com alguns links.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,28 @@
 export default function Footer() {
-  return;
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/eduardovlemes/" target="_blank">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://pt-br.facebook.com/" target="_blank">
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/" target="_blank">
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/" target="_blank">
+            YouTube
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
