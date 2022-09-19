1ce2db4fabc61fbd23b658b144c3b2ae6c0cb50d
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-19 01:28:14 -0300

    feat: adiciona Mapa
    Adiciona mapa e farmácias cadastradas.

--- src/components/PharmacyMap/PharmacyMap.js
+++ src/components/PharmacyMap/PharmacyMap.js
@@ -1,7 +1,74 @@
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 export default function PharmacyMap() {
+  const [pharmacies, setPharmacies] = useState([]);
+
+  useEffect(() => {
+    async function getPharmacies() {
+      await fetch("http://localhost:3001/pharmacies")
+        .then((response) => response.json())
+        .then((dataFromPharmaciesServer) => {
+          setPharmacies(dataFromPharmaciesServer);
+        });
+    }
+    getPharmacies();
+  }, []);
+
   return (
     <>
-      <h1>Mapa</h1>
+      <h1>Farmácias cadastradas</h1>
+      <MapContainer
+        center={[-13.747492, -53.470339]}
+        zoom={4}
+        scrollWheelZoom={true}
+        style={{ height: "500px", width: "500px" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {pharmacies.map((pharmacy) => (
+          <Marker position={[pharmacy.latitude, pharmacy.longitude]}>
+            <Popup>
+              <>
+                <p>
+                  <strong>Razão Social: </strong>
+                  {pharmacy.corporateName}
+                </p>
+                <p>
+                  <strong>CNPJ: </strong>
+                  {pharmacy.cnpj}
+                </p>
+                <p>
+                  <strong>Nome Fantasia: </strong>
+                  {pharmacy.tradeName}
+                </p>
+                <p>
+                  <strong>Email: </strong>
+                  {pharmacy.email}
+                </p>
+                <p>
+                  <strong>Telefone:</strong>
+                  {pharmacy.phone}
+                </p>
+                <p>
+                  <strong>Celular: </strong>
+                  {pharmacy.cellphone}
+                </p>
+                <span>CEP: {pharmacy.cep}</span>
+                <span>Rua: {pharmacy.street}</span>
+                <span>Número:{pharmacy.addressNumber}</span>
+                <span>Bairro: {pharmacy.district}</span>
+                <span>Cidade: {pharmacy.city}</span>
+                <span>UF: {pharmacy.federativeUnit}</span>
+                <p>Complemento: {pharmacy.addressCompl}</p>
+              </>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </>
   );
 }
