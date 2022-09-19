08e31d94e03a8cdc20ca63c53e784d2ad1c0876c
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-19 15:38:04 -0300

    feat: adiciona header
    Adiciona o componente header

--- src/pages/PharmacyRegistrationPage/index.js
+++ src/pages/PharmacyRegistrationPage/index.js
@@ -1,8 +1,13 @@
+import { useNavigate } from "react-router";
+import Header from "../../components/Header/Header";
 import PharmacyForm from "../../components/PharmacyForm/PharmacyForm";
 
 export default function PharmacyRegistrationPage() {
+  const navigate = useNavigate();
+
   return (
     <>
+      <Header />
       <PharmacyForm />
     </>
   );
