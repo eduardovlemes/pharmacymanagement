8cbb4a84096671ba51bcdb91669e211b3a647251
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-18 22:20:46 -0300

    feat: remove lógica e deixa componente lista
    Remove a lógica e deixa apenas o componente lista para ser renderizado quando chamado.

--- src/components/DrugItem/DrugItem.js
+++ src/components/DrugItem/DrugItem.js
@@ -1,34 +1,20 @@
-import { useEffect, useState } from "react";
-
-export default function DrugItem() {
-  const [drugs, setDrugs] = useState([]);
-
-  useEffect(() => {
-    async function getDrugs() {
-      await fetch("http://localhost:3001/drugs")
-        .then((response) => response.json())
-        .then((dataFromDrugsServer) => {
-          setDrugs(dataFromDrugsServer);
-        });
-    }
-    getDrugs();
-  }, []);
-
+export default function DrugItem({ value }) {
   return (
     <>
-      <h2>Medicamentos cadastrados</h2>
       <ul>
-        {drugs.map((item) => (
-          <li>
-            <img alt="drug" src={item.imgDrug} />
-            <div>
-              <span>{item.drugName}</span>
-              <span>{item.dosage}</span>
-              <p>R$ {item.price}</p>
-              <p>{item.lab}</p>
-            </div>
-          </li>
-        ))}
+        <li>
+          <img
+            alt={"Medicamento" + value.drugName}
+            src={value.imgDrug}
+            width={100}
+          />
+          <div>
+            <span>{value.drugName}</span>
+            <span>{value.dosage}</span>
+            <p>R$ {value.price}</p>
+            <p>{value.lab}</p>
+          </div>
+        </li>
       </ul>
     </>
   );
