164ee93ad94af7ee6a69cc3b12175830535ee0aa
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-19 22:46:54 -0300

    feat: adiciona modal no DrugDetail
    Adiciona modal que apresenta todas as informações do medicamento.

--- src/components/DrugDetail/DrugDetail.js
+++ src/components/DrugDetail/DrugDetail.js
@@ -1,13 +1,54 @@
-export default function DrugDetail() {
-  return(
-    <>
-    <span>Medicamento:</span>
-    <span>Tipo:</span>
-    <span>Laboratório:</span>
-    <span>Preço:</span>
-    <span>Dosagem:</span>
-    <p>Descrição:</p>
-    <buttun>Fechar</buttun>
-    </>
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function DrugDetail({ value }) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Ver detalhes</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {value.drugName}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <img
+              alt={"Medicamento" + value.drugName}
+              src={value.imgDrug}
+              width={100}
+            />
+            <span>{value.drugName}</span>
+            <span>{value.dosage}</span>
+            <p>R$ {value.price}</p>
+            <p>{value.lab}</p>
+            <p>{value.type}</p>
+            <p>{value.description}</p>
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
   );
 }
