bb915b092b950f93a1a27fe5426630419e27ffb7
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-21 16:23:44 -0300

    feat: cria página UserRegistrationPage
    Cria a página para registro de usuários

---
+++ src/pages/UserRegistrationPage/index.js
@@ -0,0 +1,13 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import UserForm from "../../components/UserForm/UserForm";
+
+export default function UserRegistrationPage() {
+  return (
+    <>
+      <Header />
+      <UserForm />
+      <Footer />
+    </>
+  );
+}
