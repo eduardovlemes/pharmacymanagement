06b349fb9fa967565b4d601f66ce1be0d0026900
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-22 15:40:39 -0300

    feat: adiciona Header e Footer no NotFound
    Adiciona os componentes Header e Footer no NotFound para o usuário clicar na página que gostaria de ir.

--- src/pages/NotFoundPage/index.js
+++ src/pages/NotFoundPage/index.js
@@ -1,9 +1,13 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
 import NotFound from "../../components/NotFound/NotFound";
 
 export default function NotFoundPage() {
   return (
     <>
+      <Header />
       <NotFound />
+      <Footer />
     </>
   );
 }
